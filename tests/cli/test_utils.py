8ffe3840882eb9fe04ff59d776df40854739fa96
Jeroen Meidam <jeroen.meidam@jumbo.com>
2023-05-18 17:29:34 +0200

    Formats file

--- tests/cli/test_utils.py
+++ tests/cli/test_utils.py
@@ -2,7 +2,7 @@ from dbfsps.cli import utils
 
 
 def test_create_requirements_file(mocker):
-    f_check_call = mocker.patch('dbfsps.cli.utils.subprocess.check_call')
+    f_check_call = mocker.patch("dbfsps.cli.utils.subprocess.check_call")
     utils.create_requirements_file()
     args = ["poetry", "export", "-f", "requirements.txt", "--output", "requirements.txt"]
     f_check_call.assert_called_once_with(args)
